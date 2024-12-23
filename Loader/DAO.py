a6c095c82c395f40b338c1d391808a5525b7a462
Ayoub Youssoufi <59065892+ayoubyoussoufi@users.noreply.github.com>
2024-12-23 11:10:36 +0100

    Update DAO.py

--- Loader/DAO.py
+++ Loader/DAO.py
@@ -1,12 +1,11 @@
 # coding: utf-8
 
-from __future__ import division
 from __future__ import unicode_literals
-
+from __future__ import division
 from sqlalchemy import *
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import mapper
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import yaml
 from path import Path
@@ -18,72 +17,122 @@ with open(config_file, 'r') as file:
     try:
         config = yaml.safe_load(file)
     except yaml.YAMLError as exc:
-        # Use format() for string formatting in Python 3.5
-        print("Error in configuration file: {}".format(exc))
+        print("Error in configuration file: {exc}")
+
 
 
 Base = declarative_base()
 engine = create_engine(
-    'mssql+pymssql://%s:%s@%s:%s/%s' % (
-        config['_dbuser'],
-        config['_dbpassword'],
-        config['_dbserver'],
-        config['_dbport'],
-        config['_dbname']
-    ),
-    echo=False
-    #,isolation_level="READ_UNCOMMITTED"
+        'mssql+pymssql://%s:%s@%s/%s' % (
+            config['_dbuser'],
+           config['_dbpassword'],
+            config['_dbserver'],
+            #_dbport,
+            config['_dbname']
+        ),
+        echo=False
+    )
 
-)
-
-_WeekId = config.get('_WeekId', None)
-ADULT_DIST = config.get('ADULT_DIST', [])
-_OthersWeightNewsCata = config.get('_OthersWeightNewsCata', {})
-_OthersWeight = config.get('OthersWeight', {})
-# create a Session
 Session = sessionmaker(bind=engine)
 session = Session()
 
 metadata = MetaData(engine)
 
+def defaultEAN(context):
+    return context.get_current_parameters()['EAN']
+
+days = Table('Day', metadata, autoload=True)
+
+
+products = Table(
+        'Product', metadata,
+        Column('Disabled', Integer, default=0),         #looks like reflection ignores default values set in DB.  TODO: check for a better solution.
+        Column('IsAutoDisabled', Integer, default=0),
+        #Column('EAN_Libelle', String, default=defaultEAN),
+        autoload=True
+)
+loadprocesses = Table('LoadProcess', metadata, autoload=True)
+outlets = Table('Outlet', metadata,
+        Column('Disabled', Integer, default=0),
+        autoload=True
+)
+destinations = Table('Destination', metadata, autoload=True)
+nations = Table('Nation', metadata, autoload=True)
 rawdatas = Table('RawData', metadata, autoload=True)
-datadatypes = Table('DataType', metadata, autoload=True)
-sources = Table('Source', metadata, autoload=True)
-days = Table('Calendar', metadata, autoload=True)
-products = Table('Product', metadata, autoload=True)
-
-
-# These are the empty classes that will become our data classes
-class RawData(object):
-    pass
-
-
-rawdatamapper = mapper(RawData, rawdatas)
-
-
-class DataType(object):
-    pass
-
-
-datatypemapper = mapper(DataType, datadatypes)
-
-
-class Source(object):
-    pass
-
-
-sourcemapper = mapper(Source, sources)
-
+currencys = Table('Currency', metadata, autoload=True)
+extractprocesses = Table('ExtractProcess', metadata, autoload=True)
+businessassistants = Table('BusinessAssistant', metadata, autoload=True)
+sourceproductnames = Table('SourceProductNames', metadata, autoload=True)
+sourcecodificationproduct = Table('SourceCodificationProduct', metadata, autoload=True)
+sourcecodificationproducttram = Table('SourceCodificationProductTRAM', metadata, autoload=True)
+exchangerate = Table('FullExchangeRate', metadata, autoload=True)
 
 class Day(object):
     pass
 
-
 daymapper = mapper(Day, days)
-
 
 class Product(object):
     pass
 
-
 productmapper = mapper(Product, products)
+
+class SourceCodificationProduct(object):
+    pass
+
+sourcecodificationproduct = mapper(SourceCodificationProduct, sourcecodificationproduct)
+
+class SourceCodificationProductTRAM(object):
+    pass
+
+sourcecodificationproducttram = mapper(SourceCodificationProductTRAM, sourcecodificationproducttram)
+
+class BusinessAssistant(object):
+    pass
+
+businessassistantmapper = mapper(BusinessAssistant, businessassistants)
+
+class LoadProcess(object):
+    pass
+
+loadprocessmapper = mapper(LoadProcess, loadprocesses)
+
+class Outlet(object):
+    pass
+
+outletmapper = mapper(Outlet, outlets)
+
+class Destination(object):
+    pass
+
+destinationmapper = mapper(Destination, destinations)
+
+class Nation(object):
+    pass
+
+nationmapper = mapper(Nation, nations)
+
+class RawData(object):
+    pass
+
+rawdatamapper = mapper(RawData, rawdatas)
+
+class Currency(object):
+    pass
+
+currencymapper = mapper(Currency, currencys)
+
+class SourceProductNames(object):
+    pass
+sourceproductnamesmapper = mapper(SourceProductNames,sourceproductnames)
+
+class ExtractProcess(object):
+    pass
+
+extractmapper = mapper(ExtractProcess, extractprocesses)
+
+class FullExchangeRate(object):
+    pass
+
+fullexchangeratemapper = mapper(FullExchangeRate, exchangerate)
+
